7976832a8ae4a0395dee9b47a28b0ef57365f25b
caridade1706 <tiagocgomes@ua.pt>
2023-09-27 17:10:19 +0100

    guiao done

---
+++ Guião_1/Ex1.5/Atendimento_a/src/main/java/org/example/atendimento.java
@@ -0,0 +1,118 @@
+package org.example;
+
+import java.util.*;
+import redis.clients.jedis.Jedis;
+
+public class atendimento {
+    public static String USERS = "users";
+    public static String PRODUCTS = "products";
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis();
+
+        System.out.println("-----Menu-----");
+        Scanner scanner = new Scanner(System.in);
+        int escolha;
+
+        do {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1. Adicionar utilizador");
+            System.out.println("2. Adicionar produto");
+            System.out.println("3. Ver detalhe");
+            System.out.println("4. Sair");
+
+            System.out.print("Digite o número da opção desejada: ");
+            escolha = scanner.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    System.out.print("Utilizador: ");
+                    inputUser(USERS, scanner, jedis);
+                    break;
+                case 2:
+                    System.out.print("Utilizador: ");
+                    inputProduct(USERS, PRODUCTS, scanner, jedis);
+                    break;
+                case 3:
+                    System.out.print("Utilizador: ");
+                    listProduct(USERS, PRODUCTS, scanner, jedis);
+                    break;
+                case 4:
+                    System.out.println("ADEUS");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (escolha != 4);
+        scanner.close();
+    }
+
+    private static void listProduct(String USERS, String PRODUCTS, Scanner scanner, Jedis jedis) {
+        String nome = scanner.next();
+
+        if (!jedis.sismember(USERS, nome)) {
+            System.err.println("ERRO: utilizador não encontrado!");
+            return;
+        }
+
+        if (!jedis.hexists(PRODUCTS, nome)) {
+            System.err.println("ERRO: utilizador não tem produtos!");
+            return;
+        }
+
+        String produtos = jedis.hget(PRODUCTS, nome);
+        System.out.println("Produtos: " + produtos);
+    }
+
+    private static void inputProduct(String USERS, String PRODUCTS, Scanner scanner, Jedis jedis) {
+        String nome;
+        String produto;
+        List<String> produtos = new ArrayList<>();
+        nome = scanner.next();
+
+        if (!jedis.sismember(USERS, nome)) {
+            System.out.println("Utilizador não inserido");
+        } else {
+            System.out.print("Produto: ");
+            produto = scanner.next();
+
+            long currentTime = System.currentTimeMillis() / 1000; // Get current time in seconds
+
+            if (!jedis.hexists(PRODUCTS, nome)) {
+                produtos.add(produto);
+                String myproducts = String.join(",", produtos);
+                jedis.hset(PRODUCTS, nome, myproducts);
+                jedis.hset("timestamps", nome, String.valueOf(currentTime));
+            } else {
+                long lastTimestamp = Long.parseLong(jedis.hget("timestamps", nome));
+
+                // Check if 60 minutes have passed
+                if (currentTime - lastTimestamp >= 3600) {
+                    jedis.hdel(PRODUCTS, nome);
+                    jedis.hdel("timestamps", nome);
+                    jedis.hset("timestamps", nome, String.valueOf(currentTime));
+                    produtos.add(produto);
+                } else {
+                    produtos = new ArrayList<>(Arrays.asList(jedis.hget(PRODUCTS, nome).split(",")));
+                    if (produtos.size() > 30) {
+                        System.out.println("Limite de produtos atingido neste intervalo de tempo");
+                    } else {
+                        produtos.add(produto);
+                    }
+                }
+
+                String myproducts = String.join(",", produtos);
+                jedis.hset(PRODUCTS, nome, myproducts);
+            }
+        }
+    }
+
+    private static void inputUser(String USERS, Scanner scanner, Jedis jedis) {
+        String nome = scanner.next();
+        if (jedis.sismember(USERS, nome)) {
+            System.out.println("Utilizador já inserido");
+        } else {
+            jedis.sadd(USERS, nome);
+        }
+    }
+}
